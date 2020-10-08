a75c255a0e5dfcee7733f6a6def78934df2aeb70
Kamil Dziekan <dziekan30@gmail.com>
2020-10-07 22:24:52 -0500

    working on footer and font awesome

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import { Jumbotron } from 'react-bootstrap'
 
+
+
 export default function Footer() {
   return (
     <Jumbotron className="footer">
       <div>
-        <h1>Footer</h1>
+        <div>
+          <h1>Footer</h1>
+          <i class="fab fa-facebook"></i>
+
+        </div>
+        <div>
+          <p>© 2020 Rikverse.  All rights reserved. </p>
+        </div>
       </div>
     </Jumbotron>
   )
