989e150cb20f14001ab0101115b77370e20ea610
Kamil Dziekan <dziekan30@gmail.com>
2020-10-16 22:40:45 -0500

    added fontawesome

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import { Jumbotron } from 'react-bootstrap'
 
-
-
 export default function Footer() {
   return (
     <Jumbotron className="footer">
       <div>
         <div>
-          <h1>Footer</h1>
-          <i className="fab fa-facebook"></i>
-          <i className="fab fa-facebook"></i><i class="fab fa-facebook"></i>
+          <i class="fab fa-facebook-square"></i>
+          <i class="fab fa-instagram"></i>
+          <i class="fas fa-envelope-square"></i>
         </div>
         <div>
           <p>© 2020 Rikverse.  All rights reserved. </p>
