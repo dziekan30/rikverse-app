3b84c2fe7bf0e41e31b54c4e39e6015c2b423f21
Kamil Dziekan <dziekan30@gmail.com>
2020-10-19 17:08:33 -0500

    added links for facebbok

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,21 +5,24 @@ export default function Footer() {
   return (
     <Jumbotron className="footer">
       <div >
-        <div>
-          <h3 style={{ color: 'white' }}>RIK<span style={{ color: 'red' }}>VERSE</span></h3>
-        </div>
-        <div>
-          <i class="fas fa-phone-alt"></i>
-          <h4>000-111-1100</h4>
-
+        <div className="row">
+          <div className="col-6">
+            <h1 style={{ color: 'white' }}>RIK<span style={{ color: 'red' }}>VERSE</span></h1>
+          </div>
+          <div className="col-6">
+            <i class="fas fa-phone-alt"></i><span>Phone Number</span>
+            <h2>000-111-1100</h2>
+          </div>
         </div>
         <div >
-          <i className="fab fa-facebook-square fa-2x icon"></i>
+          <a href="https://www.facebook.com/">
+            <i className="fab fa-facebook-square fa-2x icon"></i>
+          </a>
           <i className="fab fa-instagram fa-2x icon"></i>
           <i className="fab fa-twitter-square fa-2x icon"></i>
           <i className="fab fa-youtube fa-2x icon"></i>
         </div>
-        <div>
+        <div className="rights">
           <p>© 2020 Rikverse.  All rights reserved. </p>
         </div>
       </div>
