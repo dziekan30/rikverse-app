dc2ca8c8323b59adef63c1d4595500639bd9cde9
Kamil Dziekan <dziekan30@gmail.com>
2020-10-21 16:06:25 -0500

    almost done with styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 
 export default function About() {
   return (
     <div className="home">
-      <h1>About</h1>
-      <p>This is Rik Page</p>
+      <Container>
+        <h1>About</h1>
+        <p>
+          Vasquez was born in Southgate and raised in Pasadena, California, son to Neftali Vasquez, who built bridges in the San Gabriel Valley in the 1930s and 1940s, and Irene Vasquez. He had nine siblings.
+          After serving in the Navy, Vasquez worked in construction during the late 1940s and early 1950s. He became increasingly familiar with and interested in the cultural and sociological aspects of the Latino community in Los Angeles, and in 1952 quit his construction job to become a writer.
+          Vasquez worked at a weekly paper in Pasadena, and subsequently worked for several newspapers in the greater Los Angeles area, including the San Gabriel Valley Tribune. While working for the Tribune, he wrote a daily column focusing on Early California history, and became an expert in the field.
+          Vasquez married for the second time in 1960, to Lucy Wilbur, a college music professor and concert pianist. It was at this time that Vasquez began writing his novel, Chicano (Doubleday, 1970, HarperCollins, 2005).
+          During the 1960s, Vasquez became increasingly active in Civil Rights issues while maintaining his work as a reporter and continuing to work on his novel. On August 29, 1970, he covered the Chicano Moratorium, where colleague Ruben Salazar was killed by police. Vasquez then replaced Salazar at the Los Angeles Times, the same year that Chicano was published.
+          In addition to his work at the Los Angeles Times, Vasquez spent much time speaking at universities and high schools across California, addressing Civil Rights issues as well as discussing other social justice issues that Chicano addressed. In 1973, Richard's wife became ill, and he dedicated much of his time to care for her. He did continue to freelance as a reporter, and published two additional books, The Giant Killer and Another Land. Vasquez died in 1990.
+          In July 2020, ABC announced the development of a television series with 20th Century Fox Television, inspired by the book Chicano. The series is being written by show-runner Natalie Chaidez, and Executive Produced by Eva Longoria, Forest Whitaker, Nina Yang Bongiovi, and Doug Pray (who worked with the author's daughter Sylvia Vasquez to bring the book to screen).
+
+      </p>
+
+      </Container>
     </div>
   )
 
