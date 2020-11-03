80af5c1e1f5895d91ee862252fb9db8547bb622e
Kamil Dziekan <dziekan30@gmail.com>
2020-11-02 20:11:46 -0600

    prep for deploy

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  // <React.StrictMode>
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
 
-  </BrowserRouter>,
-  // </React.StrictMode>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
